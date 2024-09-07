05afeb13f125d58a60a010240132a89b4a063dda
huangshuai-ustc <fjwyz@outlook.com>
2024-09-07 09:41:47 +0800

    Auto commit - 2024-09-07 09:41:47

--- Rewrite/baiduCloud.adblock.js
+++ Rewrite/baiduCloud.adblock.js
@@ -3,7 +3,7 @@
 > 应用名称：墨鱼自用百度网盘去广告脚本
 > 脚本作者：@ddgksf2013
 > 微信账号：墨鱼手记
-> 更新时间：2024-08-26
+> 更新时间：2024-09-04
 > 下载地址：https://apps.apple.com/cn/app/id547166701
 > 通知频道：https://t.me/ddgksf2021
 > 贡献投稿：https://t.me/ddgksf2013_bot
@@ -17,14 +17,14 @@
 # > 百度网盘_广告推广@ddgksf2013
 ^https?:\/\/afd\.baidu\.com\/afd\/entry url script-response-body https://gist.githubusercontent.com/ddgksf2013/f43026707830c7818ee3ba624e383c8d/raw/baiduCloud.adblock.js
 # > 百度网盘_启动弹窗@ddgksf2013
-^https?:\/\/afd\.baidu\.com\/rest\/.*\/membership\/proxy\/guide url reject-200
+^https?:\/\/.*baidu\.com\/rest\/.*\/membership\/proxy\/guide url reject-200
 # > 百度网盘_设置信息流@ddgksf2013
 ^https?:\/\/pan\.baidu\.com\/act\/v\d\/(bchannel|welfare)\/list url reject-200
 # > 百度网盘_通用广告@ddgksf2013
 ^https?:\/\/pan\.baidu\.com\/rest\/.*\/pcs\/ad url reject-200
 # > 百度网盘_活动推广@ddgksf2013
 ^https?:\/\/pan\.baidu\.com\/act\/api\/activityentry url reject-200
-# > 百度网盘_设置页游戏@ddgksf2013
+# > 百度网盘_游戏广告@ddgksf2013
 ^https:\/\/pan\.baidu\.com\/rest\/.*\/membership\/user\?method=gamecenter url script-response-body https://gist.githubusercontent.com/ddgksf2013/f43026707830c7818ee3ba624e383c8d/raw/baiduCloud.adblock.js
 # > 百度网盘_热搜list@ddgksf2013
 ^https:\/\/pan\.baidu\.com\/feed\/hotlist url reject-200
